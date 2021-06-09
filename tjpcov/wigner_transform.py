9ebd86415d8359eb530d7b014b4775be1bd2408d
sukhdeep1989 <sukhdeep1989@github.com>
2021-06-09 11:12:23 -0700

    updated some older files from yaml
    fixed logger bug in wigner transform
    
    changed theta from dict to array in wigner transform

--- tjpcov/wigner_transform.py
+++ tjpcov/wigner_transform.py
@@ -9,29 +9,74 @@ from scipy.special import eval_jacobi as jacobi
 import numpy as np
 import itertools
 
+#FIXME:
+# 1. change l->ell
+# 2. Do we need to pass logger?
+# 3. Need to add inverse transform functionality.
+
 class wigner_transform():
-    def __init__(self,theta=[],l=[],s1_s2=[(0,0)],logger=None,ncpu=None,use_window=False,**kwargs):
+    def __init__(self,theta=[],l=[],s1_s2=[(0,0)],ncpu=None): #logger=None,
+        """
+        Class to compute curved sky Hankel transforms using the wigner-d matrices.
+
+        Parameters
+        ----------
+        theta:
+            Values of angular separation, theta, at which the Hankel transform is done. Should be in radians.
+        l:
+            ell values at which the Hankel transform is done. Should be integers
+        s1_s2:
+            List of spin pairs of the tracers. Each spin pair should be a tuple. e.g. for 3X2 analysis, pass [(0,0),(0,2),(2,2),(2,-2)].
+            (0,0): (galaxy,galaxy)
+            (0,2): (galaxy,shear). (2,0) is equivalent.
+            (2,2): (shear,shear), xi+
+            (2,-2): (shear,shear), xi-
+        ncpu:
+            Number of python processes to use when computing wigner-d matrices.
+        """
         self.name='Wigner'
-        self.logger=logger
+        # self.logger=logger
+
         self.l=l
         self.grad_l=np.gradient(l)
         self.norm=(2*l+1.)/(4.*np.pi) #ignoring some factors of -1,
-                                                    #assuming sum and differences of s1,s2
-                                                    #are even for all correlations we need.
+                                     #assuming sum and differences of s1,s2
+                                     #are even for all correlations we need.
+
+        #for inverse wigner transform
+        self.grad_theta=np.gradient(theta)
+        self.inv_norm=np.sin(theta)*2*np.pi
+        self.inv_wig_norm=self.inv_norm*self.grad_theta
+
         self.wig_d={}
         self.wig_3j={}
         self.s1_s2s=s1_s2
         self.theta={}
-        # self.theta=theta
+        self.theta=theta
+
+        #compute the wigner-d matrices.
         for (s1,s2) in s1_s2:
             self.wig_d[(s1,s2)]=wigner_d_parallel(s1,s2,theta,self.l,ncpu=ncpu)
 #             self.wig_d[(s1,s2)]=wigner_d_recur(s1,s2,theta,self.l)
-            self.theta[(s1,s2)]=theta #FIXME: Ugly
+            # self.theta[(s1,s2)]=theta #FIXME: Ugly and unnecessary. Should be removed.
 
 
-    def cl_grid(self,l_cl=[],cl=[],taper=False,**kwargs):
+    def cl_grid(self,l_cl=[],cl=[],taper=False,**taper_kwargs):
+        """
+        Interpolate input C_ell in case the ell values of C_ell are different from the grid on which wigner-d matrices
+        were computed during intialization.
+
+        Parameters
+        ----------
+        cl:
+            input C_ell
+        l_cl:
+            ell at which the input C_ell is computed.
+        taper:
+            if True apply the tapering to the input C_ell. Tapering can help in reducing ringing.
+        """
         if taper:
-            sself.taper_f=self.taper(l=l,**kwargs)
+            sself.taper_f=self.taper(l=l,**taper_kwargs)
             cl=cl*taper_f
         # if l==[]:#In this case pass a function that takes k with kwargs and outputs cl
         #     cl2=cl(l=self.l,**kwargs)
@@ -41,10 +86,14 @@ class wigner_transform():
         cl2=cl_int(self.l)
         return cl2
 
-    def cl_cov_grid(self,l_cl=[],cl_cov=[],taper=False,**kwargs):
+    def cl_cov_grid(self,l_cl=[],cl_cov=[],taper=False,**taper_kwargs):
+        """
+        Same as cl_grid, but for the 2D covariance. Uses 2D interpolation.
+
+        """
         if taper:#FIXME there is no check on change in taper_kwargs
             if self.taper_f2 is None or not np.all(np.isclose(self.taper_f['l'],cl)):
-                self.taper_f=self.taper(l=l,**kwargs)
+                self.taper_f=self.taper(l=l,**taper_kwargs)
                 taper_f2=np.outer(self.taper_f['taper_f'],self.taper_f['taper_f'])
                 self.taper_f2={'l':l,'taper_f2':taper_f2}
             cl=cl*self.taper_f2['taper_f2']
@@ -57,13 +106,70 @@ class wigner_transform():
             cl2=cl_int(self.l,self.l)
         return cl2
 
-    def projected_correlation(self,l_cl=[],cl=[],s1_s2=[],taper=False,**kwargs):
-        cl2=self.cl_grid(l_cl=l_cl,cl=cl,taper=taper,**kwargs)
+    def projected_correlation(self,l_cl=[],cl=[],s1_s2=(),taper=False,**taper_kwargs):
+        """
+        Convert input C_ell to the correlation function.
+
+        Parameters
+        ----------
+        cl:
+            Input C_ell
+        l_cl:
+            ell values at which input C_ell is computer.
+        s1_s2:
+            Tuple of the spin factors of the tracers. Used to identify the correct wigner-d matrix to use.
+        taper:
+            If true, apply tapering to the input C_ell
+        taper_kwargs:
+            Arguments to be passed to the tapering function.
+        """
+        cl2=self.cl_grid(l_cl=l_cl,cl=cl,taper=taper,**taper_kwargs)
         w=np.dot(self.wig_d[s1_s2]*self.grad_l*self.norm,cl2)
-        return self.theta[s1_s2],w
+        return self.theta,w
+
+    def inv_projected_correlation(self,theta_xi=[],xi=[],s1_s2=[],taper=False,**kwargs):
+        """
+        Convert input xi to C_ell, the inverse hankel transform
+        Parameters
+        ----------
+        xi:
+            The input correlation function
+        theta_xi:
+            theta values at which xi is computed.
+        s1_s2:
+            Tuple of the spin factors of the tracers. Used to identify the correct wigner-d matrix to use.
+        """
+        wig_d=self.wig_d[s1_s2].T
+        wig_theta=self.theta
+        wig_norm=self.inv_wig_norm
+
+        xi2=self.cl_grid(l_cl=theta_xi,cl=xi,taper=taper,wig_l=wig_theta,**kwargs)
+        cl=np.dot(wig_d*wig_norm,xi2)
+        return self.l,cl
+
+
 
     def projected_covariance(self,l_cl=[],cl_cov=[],s1_s2=[],s1_s2_cross=None,
                             taper=False,**kwargs):
+        """
+        Convert C_ell covariance to correlation function.
+        This function assumes that cl_cov is one dimensional array (i.e. only defined along the diagonal),
+        useful for diagonal gaussian covariances.
+        For the 2D covariances, use projected_covariance2 function.
+
+        Parameters
+        ----------
+        cl_cov:
+            Diagonal of the covariance matrix.
+        l_cl:
+            ell values at which input C_ell is computer.
+        s1_s2:
+            Tuple of the spin factors of the first set of tracers. Used to identify the correct wigner-d matrix to use.
+        s1_s2_cross:
+            Tuple of the spin factors of the second set of tracers, if different from s1_s2.
+            Used to identify the correct wigner-d matrix to use.
+
+        """
         if s1_s2_cross is None:
             s1_s2_cross=s1_s2
         #when cl_cov can be written as vector, eg. gaussian covariance
@@ -71,11 +177,27 @@ class wigner_transform():
         cov=np.einsum('rk,k,sk->rs',self.wig_d[s1_s2]*np.sqrt(self.norm),cl2*self.grad_l,
                     self.wig_d[s1_s2_cross]*np.sqrt(self.norm),optimize=True)
         #FIXME: Check normalization
-        return self.theta[s1_s2],cov
+        return self.theta,cov
 
     def projected_covariance2(self,l_cl=[],cl_cov=[],s1_s2=[],s1_s2_cross=None,
                                 taper=False,**kwargs):
-        #when cl_cov is a 2-d matrix
+        """
+        Convert C_ell covariance to correlation function.
+        This function assumes that cl_cov is 2D matrix.
+
+        Parameters
+        ----------
+        cl_cov:
+            C_ell covariance matrix.
+        l_cl:
+            ell values at which input C_ell is computed.
+        s1_s2:
+            Tuple of the spin factors of the first set of tracers. Used to identify the correct wigner-d matrix to use.
+        s1_s2_cross:
+            Tuple of the spin factors of the second set of tracers, if different from s1_s2.
+            Used to identify the correct wigner-d matrix to use.
+        """
+
         if s1_s2_cross is None:
             s1_s2_cross=s1_s2
         cl_cov2=cl_cov  #self.cl_cov_grid(l_cl=l_cl,cl_cov=cl_cov,s1_s2=s1_s2,taper=taper,**kwargs)
@@ -85,9 +207,21 @@ class wigner_transform():
 #         cov=np.dot(self.wig_d[s1_s2]*self.grad_l*np.sqrt(self.norm),np.dot(self.wig_d[s1_s2_cross]*np.sqrt(self.norm),cl_cov2).T)
         # cov*=self.norm
         #FIXME: Check normalization
-        return self.theta[s1_s2],cov
+        return self.theta,cov
 
     def taper(self,l=[],large_k_lower=10,large_k_upper=100,low_k_lower=0,low_k_upper=1.e-5):
+        """
+        Function to apply tapering to input C_ell. Tapering is useful to reduce the ringing.
+        This function uses the cosine function to apply the tapering.
+        See eq. 71 in https://arxiv.org/pdf/2105.04548.pdf for the function and meaning of input
+        parameters.
+
+        Parameters
+        ----------
+        l:
+            ell values at which input C_ell is computed.
+        """
+
         #FIXME there is no check on change in taper_kwargs
         if self.taper_f is None or not np.all(np.isclose(self.taper_f['k'],k)):
             taper_f=np.zeros_like(k)
@@ -101,9 +235,26 @@ class wigner_transform():
         return self.taper_f
 
     def diagonal_err(self,cov=[]):
+        """
+        Returns the diagonal error from the covariance. Useful for errorbar plots.
+        """
         return np.sqrt(np.diagonal(cov))
 
 def wigner_d(s1,s2,theta,l,l_use_bessel=1.e4):
+    """
+    Function to compute the wigner-d matrices
+    Parameters
+    ----------
+    s1,s2:
+        Spin factors for the wigner-d matrix.
+    theta:
+        Angular separation for which to compute the wigner-d matrix. The matrix depends on cos(theta).
+    l:
+        The spherical harmonics mode ell for which to compute the matrix.
+    l_use_bessel:
+        Due to numerical issues, we need to switch from wigner-d matrix to bessel functions at high ell (see the note below).
+        This defines the scale at which the switch happens.
+    """
     l0=np.copy(l)
     if l_use_bessel is not None:
     #FIXME: This is not great. Due to a issues with the scipy hypergeometric function,
@@ -143,6 +294,15 @@ def wigner_d(s1,s2,theta,l,l_use_bessel=1.e4):
 
 
 def wigner_d_parallel(s1,s2,theta,l,ncpu=None,l_use_bessel=1.e4):
+    """
+    compute the wigner-d matrix in parallel using multiprocessing Pool.
+    This function calls the wigner-d function defined above.
+
+    Parameters
+    ----------
+    ncpu:
+        number of processes to use for computing the matrix.
+    """
     if ncpu is None:
         ncpu=cpu_count()
     p=Pool(ncpu)
@@ -151,6 +311,19 @@ def wigner_d_parallel(s1,s2,theta,l,ncpu=None,l_use_bessel=1.e4):
     return d_mat[:,:,0].T
 
 def bin_mat(r=[],mat=[],r_bins=[]):#works for cov and skewness
+        """
+        Function to apply the binning operator. This function works on both one dimensional vectors
+        and two dimensional covariance matrices.
+
+        Parameters
+        ----------
+        r:
+            theta or ell values at which the un-binned vector is computed.
+        mat:
+            Unbinned vector of C_ell or xi or the unbinned covariance
+        r_bins:
+            theta or ell bins to which the values should be binned.
+        """
         bin_center=0.5*(r_bins[1:]+r_bins[:-1])
         n_bins=len(bin_center)
         ndim=len(mat.shape)
@@ -191,13 +364,16 @@ def bin_mat(r=[],mat=[],r_bins=[]):#works for cov and skewness
             mat_int[indxs]=np.sum(mat_t)/norm_ijk
             norm_int[indxs]=norm_ijk
         return bin_center,mat_int
-    
+
 def bin_cov(r=[], cov=[], r_bins=[]):
+    """
+    A slower function to test the bin_mat function above.
+    """
     bin_center = 0.5*(r_bins[1:]+r_bins[:-1])
     n_bins = len(bin_center)
     cov_int = np.zeros((n_bins, n_bins), dtype='float64')
     bin_idx = np.digitize(r, r_bins)-1
-    
+
     # this takes care of problems around bin edges
     r2 = np.sort(np.unique(np.append(r, r_bins)))
     dr = np.gradient(r2)
