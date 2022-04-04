ce073b3025278873be1058f07ba7cc8bed05159c
carlosggarcia <carlos.garcia-garcia@physics.ox.ac.uk>
2022-04-04 17:56:29 +0100

    test read maps

--- tjpcov/tools.py
+++ tjpcov/tools.py
@@ -18,9 +18,9 @@ def read_map_from_hdf5(fname, name, nside):
 
 def read_map(fname, name=None, nside=None):
     if h5py.is_hdf5(fname):
-        if nside is None:
+        if (nside is None) or (name is None):
             raise ValueError('If masks are in hdf5 format, you need to pass ' +
-                             'the nside of the maps')
+                             'the nside and name of the maps')
         m = read_map_from_hdf5(fname, name, nside)
     else:
         m = hp.read_map(fname)
