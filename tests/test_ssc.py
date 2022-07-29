015d78a95f9818542ca7481b08f58a4fb19840fe
carlosggarcia <carlos.garcia-garcia@physics.ox.ac.uk>
2022-07-29 18:58:37 +0100

    ssc: add test comparing against Joachimi's cov (David's comments)

--- tests/test_ssc.py
+++ tests/test_ssc.py
@@ -200,6 +200,96 @@ def test_get_SSC_cov(tracer_comb1, tracer_comb2):
     assert np.all(covf['cov'] == cov_ssc_zb)
 
 
+def test_get_SSC_cov_WL_with_benchmark():
+    # Based on CCL benchmark test in benchmarks/test_covariances.py
+    #
+    # Compare against Benjamin Joachimi's code. An overview of the methodology
+    # is given in appendix E.2 of 2007.01844.
+    #
+    # The approximation is different so one cannot expect a perfect agreement.
+    # However an agreement ~5% should be enough to convince ourselves that
+    # get_SSC_cov is doing what it's supposed to do.
+
+    # Read benchmark data
+    data_dir = "tests/benchmarks/SSC/"
+    z, nofz = np.loadtxt(os.path.join(data_dir, "ssc_WL_nofz.txt"),
+                         unpack=True)
+    ell = np.loadtxt(os.path.join(data_dir, "ssc_WL_ell.txt"))
+    cov_ssc_bj = np.loadtxt(os.path.join(data_dir, "ssc_WL_cov_matrix.txt"))
+
+    # TJPCov SSC
+    cc  = get_CovarianceCalculator()
+    h = 0.7
+    cosmo = ccl.Cosmology(Omega_c=0.25, Omega_b=0.05, h=h, n_s=0.97,
+                          sigma8=0.8, m_nu=0.0)
+    cc.cosmo = cosmo
+
+    # Trick TJPCov to use the ells we want instead the ones in the file
+    s = sacc.Sacc()
+    s.tracers = cc.cl_data.tracers.copy()
+    for dt in ['cl_ee', 'cl_eb', 'cl_be', 'cl_bb']:
+        s.add_ell_cl(dt, 'DESwl__0', 'DESwl__0', ell, np.ones_like(ell))
+
+
+    # Generate also a disc mask that covers fsky = 0.05. sigma2_B agrees up to
+    # ~1%
+    radius = 0.455
+    nside = 128
+    ix = hp.query_disc(nside=nside, vec=(0, -60, 0), radius=radius)
+    mask = np.zeros(hp.nside2npix(nside))
+    mask[ix] = 1
+
+    # Modify tjpcov instance
+    cc.cl_data = s
+    cc.mask_fn['DESwl__0'] = mask
+
+    trs = ('DESwl__0', 'DESwl__0')
+    WL_tracer = ccl.WeakLensingTracer(cosmo, (z, nofz))
+    cov_ssc = cc.get_SSC_cov(trs, trs, {'DESwl__0': WL_tracer},
+                             include_b_modes=False)
+
+    clean_tmp()
+    # Tests
+    var_ssc_ccl = np.diag(cov_ssc)
+    off_diag_1_ccl = np.diag(cov_ssc, k=1)
+
+    # At large scales, CCL uses a different convention for the Limber
+    # approximation. This factor accounts for this difference
+    ccl_limber_shear_fac = np.sqrt((ell-1)*ell*(ell+1)*(ell+2))/(ell+1/2)**2
+    cov_ssc_bj_corrected = cov_ssc_bj * np.outer(ccl_limber_shear_fac**2,
+                                                 ccl_limber_shear_fac**2)
+    var_bj = np.diag(cov_ssc_bj_corrected)
+    off_diag_1_bj = np.diag(cov_ssc_bj_corrected, k=1)
+
+
+    # TODO: After the refactoring, and freeing some of the halo model choices
+    # for the SSC, we should update this test to a greater accuracy.
+    #
+    # Note: the array of a in the CCL test: a = np.linspace(1/(1+6), 1, 100)
+    #
+    #   - If I use the halomod_Tk3D_SSC, the same mass function
+    #   (MassFuncTinker10, instead of MassFuncTinker08, as is currently in
+    #   tjpcov/main.py) and the array of a in the CCL test, the test passes
+    #   with the same precision as in CCL (<3%)
+    #  - If I change the mass function to MassFuncTinker08, the error goes up
+    #  to 6.8%
+    #  - If I use the halomod_Tk3D_SSC_linear_bias, MassFuncTinker10 and the
+    #  same a's as in the test, the error goes to ~6%. (This is the error
+    #  introduced by the approximation, then)
+    #  - If I use the halomod_Tk3D_SSC_linear_bias, MassFuncTinker08 and the
+    #  same a's as in the test, the test passes with <3% error
+    #  - If I use the a's as implemented in tjpcov/main.py (i.e. from the pk
+    #  splines): the error goes up to ~18% irrespectively of the Tk3D, or mass
+    #  function that one choses. That's why the threshold is 0.2
+    #
+    # This error instability seem to appear only on the first ell. So I will do
+    # the test without including the first column & row of the covariance.
+
+    assert np.max(np.fabs(var_ssc_ccl/var_bj - 1)[1:]) < 0.07
+    assert np.max(np.fabs(off_diag_1_ccl/off_diag_1_bj - 1)[1:]) < 0.07
+    assert np.max(np.fabs(cov_ssc/cov_ssc_bj_corrected - 1)[1:][:, 1:]) < 0.07
+
+
 def test_get_all_cov_SSC():
     # Smoke
     cc  = get_CovarianceCalculator()
