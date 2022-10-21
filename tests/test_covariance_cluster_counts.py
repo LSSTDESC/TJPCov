714d6adcad3f2d567c01433b10fa3a9ca9b1fb5e
Matt Kwiecien <mattkwiecien@gmail.com>
2022-10-21 07:23:56 -0700

    Added black linting to mock test file.

--- tests/test_covariance_cluster_counts.py
+++ tests/test_covariance_cluster_counts.py
@@ -16,13 +16,14 @@ import shutil
 
 # INPUT
 # CCL and sacc input:
-os.makedirs('tests/tmp/', exist_ok=True)
+os.makedirs("tests/tmp/", exist_ok=True)
 cosmo_filename = "tests/data/cosmo_desy1.yaml"
 cosmo = ccl.Cosmology.read_yaml(cosmo_filename)
 
 # SETUP
 input_yml = "examples/clusters/tjpcov_conf_minimal_clusters.yaml"
 cluster_count_cov = CovarianceClusterCounts(input_yml)
+
 
 def get_config():
     return CovarianceIO(input_yml).config
@@ -32,8 +33,5 @@ def test_smoke():
     cc_cov = CovarianceClusterCounts(input_yml)
 
     def analytic_bessel_fn(xi, Beta):
-        prefactor = (1/(2 * np.pi * np.sqrt(np.pi * Beta) * (4*xi**2)))
-        return prefactor * (1-np.exp(-(xi**2)/Beta))
-
-    
-
+        prefactor = 1 / (2 * np.pi * np.sqrt(np.pi * Beta) * (4 * xi**2))
+        return prefactor * (1 - np.exp(-(xi**2) / Beta))
