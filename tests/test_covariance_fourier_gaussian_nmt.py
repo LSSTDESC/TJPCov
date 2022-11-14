a55abeb1144a95b5c4b968dc17b2cb2797251935
carlosggarcia <carlos.garcia-garcia@physics.ox.ac.uk>
2022-11-14 10:22:23 +0000

    blacked

--- tests/test_covariance_fourier_gaussian_nmt.py
+++ tests/test_covariance_fourier_gaussian_nmt.py
@@ -357,7 +357,7 @@ def test_get_cl_for_cov():
     cl_fid = get_cl("galaxy_clustering", fiducial=True)
     cl_fid_Sh = get_cl("galaxy_shear", fiducial=True)
 
-    cl_cp = (w.couple_cell(cl_fid["cl"]) + cl["nl_cp"]) / np.mean(m ** 2)
+    cl_cp = (w.couple_cell(cl_fid["cl"]) + cl["nl_cp"]) / np.mean(m**2)
     cl_cp_code = cnmt.get_cl_for_cov(
         cl_fid["cl"], cl["nl_cp"], m, m, w, nl_is_cp=True
     )
