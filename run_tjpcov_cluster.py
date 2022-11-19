9d2e8cccf54db833e18d279bb6f4c11705b1cab7
Matt Kwiecien <mattkwiecien@gmail.com>
2022-11-19 08:51:12 -0800

    Changes to make the initial example work

--- run_tjpcov_cluster.py
+++ run_tjpcov_cluster.py
@@ -2,18 +2,22 @@
 
 import os, sys
 
-sys.path.append("/global/u1/m/mkwiecie/desc/repos/TJPCov")
-sys.path.append("/global/u1/m/mkwiecie/.local/lib/python3.8/site-packages")
+# /Users/matt/Repos/TJPCov
+# sys.path.append("/global/u1/m/mkwiecie/desc/repos/TJPCov")
+# sys.path.append("/global/u1/m/mkwiecie/.local/lib/python3.8/site-packages")
+sys.path.append("/Users/matt/Repos/TJPCov")
 
 import pickle
 from tjpcov_new.covariance_cluster_counts import CovarianceClusterCounts
 
 if __name__ == "__main__":
 
-    input_yml = "/global/u1/m/mkwiecie/desc/repos/TJPCov/examples/clusters/tjpcov_conf_minimal_clusters.yaml"
+    # input_yml = "/global/u1/m/mkwiecie/desc/repos/TJPCov/examples/clusters/tjpcov_conf_minimal_clusters.yaml"
+    input_yml = "/Users/matt/Repos/TJPCov/examples/clusters/tjpcov_conf_minimal_clusters.yaml"
     cov = CovarianceClusterCounts(input_yml)
     cov_nxn = cov.get_covariance()
 
-    fname = "/global/u1/m/mkwiecie/desc/repos/TJPCov/examples/clusters/cov_nxn_mpi_large.pkl"
+    # fname = "/global/u1/m/mkwiecie/desc/repos/TJPCov/examples/clusters/cov_nxn_mpi_large.pkl"
+    fname = "/Users/matt/Repos/TJPCov/examples/clusters/cov_nxn_mpi_large.pkl"
     with open(fname, "wb") as ff:
         pickle.dump(cov_nxn, ff)
