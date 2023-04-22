1d7cde6f8b6aba0f1b97c69c23eda041fdbf649e
Matt Kwiecien <mattkwiecien@gmail.com>
2023-04-22 09:57:37 -0700

    Adding teardown/setup modules

--- tests/test_covariance_io.py
+++ tests/test_covariance_io.py
@@ -8,84 +8,86 @@ import shutil
 from datetime import datetime
 from glob import glob
 
-
-root = "./tests/benchmarks/32_DES_tjpcov_bm/"
-outdir = root + "tjpcov_tmp/"
-input_yml = os.path.join(root, "conf_covariance_gaussian_fourier_nmt.yaml")
-input_sacc = sacc.Sacc.load_fits(root + "cls_cov.fits")
+ROOT_DIR = "./tests/benchmarks/32_DES_tjpcov_bm/"
+OUT_DIR = ROOT_DIR + "tjpcov_tmp/"
+INPUT_YML = os.path.join(ROOT_DIR, "conf_covariance_gaussian_fourier_nmt.yaml")
 
 
-def clean_tmp():
-    if os.path.isdir(outdir):
-        shutil.rmtree(outdir)
-        os.makedirs(outdir)
+def teardown_module():
+    """whole test run finishes."""
+    shutil.rmtree(OUT_DIR)
 
 
-# Cleaning the tmp dir before running and after running the tests
-@pytest.fixture(autouse=True)
-def run_clean_tmp():
-    clean_tmp()
+def setup_module():
+    os.makedirs(OUT_DIR, exist_ok=True)
 
 
-def get_diag_covariance():
-    ndata = input_sacc.mean.size
+@pytest.fixture
+def mock_sacc():
+    input_sacc = sacc.Sacc.load_fits(ROOT_DIR + "cls_cov.fits")
+    return input_sacc
+
+
+@pytest.fixture
+def mock_covariance_io():
+    return CovarianceIO(INPUT_YML)
+
+
+def get_diag_covariance(sacc):
+    ndata = sacc.mean.size
     return np.diag(np.ones(ndata))
 
 
 def test_smoke_input():
     # CovarianceIO should accept dictionary or file path
-    CovarianceIO(input_yml)
-    config = CovarianceIO._parse(input_yml)
+    CovarianceIO(INPUT_YML)
+    config = CovarianceIO._parse(INPUT_YML)
     CovarianceIO(config)
     with pytest.raises(ValueError):
         CovarianceIO(["hello"])
 
     # Check outdir is created
-    if os.path.isdir(outdir):
-        os.system(f"rm -rf {outdir}")
-    CovarianceIO(input_yml)
-    assert os.path.isdir(outdir)
+    if os.path.isdir(OUT_DIR):
+        os.system(f"rm -rf {OUT_DIR}")
+    CovarianceIO(INPUT_YML)
+    assert os.path.isdir(OUT_DIR)
 
 
-def test_create_sacc_cov():
-    cio = CovarianceIO(input_yml)
-    # Circunvent the NotImplementedError
-    cov = get_diag_covariance()
-    s = cio.create_sacc_cov(cov)
-    s2 = sacc.Sacc.load_fits(outdir + "cls_cov.fits")
+def test_create_sacc_cov(mock_covariance_io, mock_sacc):
+    cov = get_diag_covariance(mock_sacc)
+    s = mock_covariance_io.create_sacc_cov(cov)
+    s2 = sacc.Sacc.load_fits(OUT_DIR + "cls_cov.fits")
 
-    assert np.all(s.mean == input_sacc.mean)
-    assert np.all(s.covariance.covmat == get_diag_covariance())
+    assert np.all(s.mean == mock_sacc.mean)
+    assert np.all(s.covariance.covmat == get_diag_covariance(mock_sacc))
     assert np.all(s.mean == s2.mean)
     assert np.all(s.covariance.covmat == s2.covariance.covmat)
 
     # Check that it also writes the file with a different name
-    s2 = cio.create_sacc_cov(cov, "cls_cov2.fits")
-    s2 = sacc.Sacc.load_fits(outdir + "cls_cov2.fits")
+    s2 = mock_covariance_io.create_sacc_cov(cov, "cls_cov2.fits")
+    s2 = sacc.Sacc.load_fits(OUT_DIR + "cls_cov2.fits")
 
     # Check that it will not overwrite a file but create a new one with the utc
     # time stamped
-    s = cio.create_sacc_cov(cov)
+    s = mock_covariance_io.create_sacc_cov(cov)
     date = datetime.utcnow()
     # Timestamp without the seconds since there can be a delay between this
     # timestamp and the one when creating the sacc file.
     timestamp = date.strftime("%Y%m%d%H%M")
-    files = glob(outdir + f"cls_cov.fits_{timestamp}*")
+    files = glob(OUT_DIR + f"cls_cov.fits_{timestamp}*")
     assert len(files) == 1
 
     # Check that it will overwrite if overwrite is True
-    cio.create_sacc_cov(0 * cov, overwrite=True)
-    s2 = sacc.Sacc.load_fits(outdir + "cls_cov.fits")
+    mock_covariance_io.create_sacc_cov(0 * cov, overwrite=True)
+    s2 = sacc.Sacc.load_fits(OUT_DIR + "cls_cov.fits")
     assert np.all(s2.covariance.covmat == 0)
 
 
-def test_get_outdir():
-    cio = CovarianceIO(input_yml)
-    assert os.path.samefile(cio.get_outdir(), outdir)
+def test_get_outdir(mock_covariance_io):
+    assert os.path.samefile(mock_covariance_io.get_outdir(), OUT_DIR)
 
 
-def test_get_sacc_file():
-    cio = CovarianceIO(input_yml)
-    s = cio.get_sacc_file()
+def test_get_sacc_file(mock_covariance_io, mock_sacc):
+    s = mock_covariance_io.get_sacc_file()
 
-    assert np.all(s.mean == input_sacc.mean)
+    assert np.all(s.mean == mock_sacc.mean)
