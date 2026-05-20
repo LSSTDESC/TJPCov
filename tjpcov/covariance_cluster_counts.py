6eb60b457295ba531a77dc6641a6fcc2bc3018be
eduardojsbarroso <eduardojsbarroso@gmail.com>
2026-05-20 09:50:52 +0200

    Fix float returning value

--- tjpcov/covariance_cluster_counts.py
+++ tjpcov/covariance_cluster_counts.py
@@ -218,7 +218,7 @@ class CovarianceClusterCounts(CovarianceBuilder):
             ln_true_mass / np.log(10), z, rich_bin
         )
 
-        return result
+        return float(result)
 
     def mass_richness_integral(self, z, richness_i, remove_bias=False):
         """Integrates the HMF weighted by mass-richness relation.
